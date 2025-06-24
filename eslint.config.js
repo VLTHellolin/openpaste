4e69606881c6d2eab7d198bd70623ccb127eb8f2
VLTHellolin <i@hellolin.top>
2025-06-24 14:12:35 +0800

    feat: init

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import defineConfig from '@hellolin/eslint-config';
+
+export default defineConfig({
+  javascript: {
+    env: {
+      browser: true,
+      node: true,
+    },
+  },
+  react: true,
+});
